d9d9c10e068943a6cd1f43627c7c5cf874bec669
utfprequipe6 <emaildezoar@gmail.com>
2016-12-06 11:54:25 -0200

    Comandos Uteis para eth0 Linux

---
+++ comandosUteis.sh
@@ -0,0 +1,12 @@
+#bin/bash
+
+#alguns comandos úteis:
+
+sudo ifconfig eth0 down
+sudo ifconfig eth0 up
+
+sudo route -A inet6 add aaaa::/64 gw bbbb::100
+
+ping6 aaaa::0212:4B00:07B9:5ECC
+
+ipconfig
